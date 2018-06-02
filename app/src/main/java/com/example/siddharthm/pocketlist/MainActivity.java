b0e2af108e102f83da35020ce0f7169c7275e7c6
Siddharth M <siddharth.m98@gmail.com>
2018-06-03 02:00:19 +0530

    Adding and clearing feature added

--- app/src/main/java/com/example/siddharthm/pocketlist/MainActivity.java
+++ app/src/main/java/com/example/siddharthm/pocketlist/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.siddharthm.pocketlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    EditText t1;
+    Button add;
+    Button clear;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1 = (EditText) findViewById(R.id.editText);
+        add = (Button)findViewById(R.id.button2);
+        clear = (Button)findViewById(R.id.button);
+        list = (ListView)findViewById(R.id.list);
+        final ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemsAdapter.add(t1.getText().toString());
+                list.setAdapter(itemsAdapter);
+
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t1.setText("");
+            }
+        });
+
     }
 }
