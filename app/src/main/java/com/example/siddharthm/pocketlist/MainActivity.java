f4efbb187daf0021994bd2f8d890392fc28bdd7e
Siddharth M <siddharth.m98@gmail.com>
2018-06-08 22:34:35 +0530

    Delete individual item on longPress

--- app/src/main/java/com/example/siddharthm/pocketlist/MainActivity.java
+++ app/src/main/java/com/example/siddharthm/pocketlist/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.siddharthm.pocketlist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         clearList = (Button)findViewById(R.id.button3);
         clear = (Button)findViewById(R.id.button);
         list = (ListView)findViewById(R.id.list);
+
         final ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,13 +53,15 @@ public class MainActivity extends AppCompatActivity {
                list.setAdapter(itemsAdapter);
             }
         });
-        list.setOnLongClickListener(new View.OnLongClickListener() {
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                itemsAdapter.remove(String.valueOf(0));
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                itemsAdapter.remove(itemsAdapter.getItem(position));
+               itemsAdapter.notifyDataSetChanged();
                 return true;
             }
         });
+
 
 
     }
